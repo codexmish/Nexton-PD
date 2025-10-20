3c2a9e8458d90b62a3270e2fd925d6ed5471fd6a
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-20 19:07:25 +0600

    dark mode toggle added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,14 +8,23 @@ import { BsCartDash } from "react-icons/bs";
 const NavBar = () => {
   const [theme, setTheme] = useState("light");
 
+  const handleTheme = ()=>{
+    const newTheme = theme==='light'? 'dark': 'light'
+    setTheme(newTheme)
+    document.documentElement.classList.toggle('dark')
+
+  }
+
   return (
     <>
-      <nav>
+      <nav className="dark:bg-primary">
         <div className="main container flex items-center justify-between py-7">
+          {/* --------Logo----- */}
           <div className="logo w-[119px]">
             <img src={theme === "light" ? logo : darkLogo} alt="logo" />
           </div>
 
+          {/* ------search option------ */}
           <div className="search-box flex items-center gap-3 w-100 h-13 bg-gray-bg rounded-full text-primary text-sm font-normal font-popppins px-6 py-4">
             <CiSearch className="text-body-text text-xl" />
             <input
@@ -25,15 +34,29 @@ const NavBar = () => {
             />
           </div>
 
+          {/* --------cart and user----- */}
           <div className="icons flex items-center gap-5">
             <div className="person text-2xl text-body-text">
               <RiUserLine />
             </div>
             <div className="cart text-2xl text-body-text relative">
-              <BsCartDash /> 
-              <div className="absolute flex items-center justify-center h-5 w-5 rounded-full bg-vibrant-blu text-xs text-white font-medium font-popppins top-[-9px] right-[-6px]">4</div>
+              <BsCartDash />
+              <div className="absolute flex items-center justify-center h-5 w-5 rounded-full bg-vibrant-blu text-xs text-white font-medium font-popppins top-[-9px] right-[-6px]">
+                4
+              </div>
             </div>
+
+
+            {/* ---------Theme Toggle------- */}
+
+          <div>
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input className="sr-only peer" value="" type="checkbox" />
+              <div onClick={handleTheme} className="w-24 h-12 rounded-full ring-0 peer duration-500 outline-none bg-gray-200 overflow-hidden before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center before:content-['☀️'] before:absolute before:h-10 before:w-10 before:top-1/2 before:bg-white before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-700 peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-[#383838] after:content-['🌑'] after:absolute after:bg-[#1d1d1d] after:rounded-full after:top-[4px] after:right-1 after:translate-y-full after:w-10 after:h-10 after:opacity-0 after:transition-all after:duration-700 peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:translate-y-0"></div>
+            </label>
           </div>
+          </div>
+
         </div>
       </nav>
     </>
