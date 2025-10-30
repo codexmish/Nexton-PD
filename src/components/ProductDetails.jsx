d34acc6bf984955578e34ae36df42d41d47bbc43
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-30 17:15:06 +0600

    product details skeliton error

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -126,111 +126,119 @@ const ProductDetails = () => {
               </div>
             )}
 
-            {/* ------------Product options part----- */}
-            <div className="product-opt w-[290px] lg:w-[460px] p-2 lg:p-8 lg:border border-[#E5E7EB] rounded-2xl h-fit">
-              <h2 className="text-2xl text-primary dark:text-white font-semibold font-popppind lg:hidden">
-                {spro.title}
-              </h2>
-              <div className="w-full flex justify-between items-start mt-3">
-                <div className="flex items-center gap-1 order-2 lg:order-0 w-full">
-                  <FaStar className="text-[#FBBF24]" />
-                  <p className="text-[12px] lg:text-base text-secoundary dark:text-darkSecoundart font-semibold font-popppind">
-                    {spro.rating}{" "}
-                    <span className="hidden lg:inline-block">
-                      . {} reviews{" "}
-                    </span>{" "}
-                    <span className="lg:hidden">(98)</span>
-                  </p>
-                </div>
+            {/* ------------Product price and details part----- */}
 
-                <div className="price lg:text-end order-1 lg:order-0 mb-6 lg:mb-0">
-                  <h2 className="text-[20px] lg:text-2xl text-primary dark:text-white font-semibold font-popppind">
-                    $
-                    {Math.round(
-                      spro.price - (spro.price * spro.discountPercentage) / 100
-                    )}
-                  </h2>
-                  <h3 className="text-[12px] lg:text-sm text-secoundary dark:text-darkSecoundart font-medium font-popppind line-through">
-                    ${spro.price}
-                  </h3>
-                </div>
-              </div>
-
-              {/* -----Product Size----- */}
-
-              <h2 className="text-base text-primary dark:text-white font-semibold font-popppind">
-                Size: S
-              </h2>
-              <div className="flex flex-wrap gap-2 mt-3 mb-8 text-primary dark:text-white">
-                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
-                  S
-                </button>
-                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
-                  M
-                </button>
-                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
-                  L
-                </button>
-                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
-                  XL
-                </button>
-                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
-                  2XL
-                </button>
-              </div>
-
-              {/* -------Add To Cart Option---- */}
-              <div className="flex justify-between">
-                {/* QTY */}
-                <div className="py-[2px] lg:py-2 px-[6px] lg:px-3 rounded-[9999px] bg-border dark:bg-primary dark:border dark:border-border flex gap-4 items-center">
-                  <button className="w-5 lg:w-6 h-5 lg:h-6 rounded-full border border-border bg-white dark:bg-primary flex justify-center items-center">
-                    <FaMinus className="text-[12px] lg:text-sm text-primary dark:text-white font-normal" />
-                  </button>
-                  <h2 className="text-base text-primary dark:text-white font-medium font-popppind">
-                    1
-                  </h2>
-                  <button className="w-5 lg:w-6 h-5 lg:h-6 rounded-full border border-border bg-white dark:bg-primary flex justify-center items-center">
-                    <FaPlus className="text-[12px] lg:text-sm text-primary dark:text-white" />
-                  </button>
-                </div>
-
-                {/* Cart button */}
-
-                <button className="py-[10px] lg:py-4 px-[24px] lg:px-8 bg-primary text-sm lg:text-base text-white font-medium font-popppind rounded-[9999px] dark:border dark:border-border flex justify-center items-center gap-2">
-                  <HiShoppingBag />
-                  Add to cart
-                </button>
-              </div>
-
-              <div className="hidden lg:block">
-                <div className="det flex items-center justify-between mt-8 pb-4 border-b border-b-border">
-                  <div className="l-text mb-[10px]">
-                    <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
-                      $169.99 x 1
-                    </p>
-                    <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
-                      Tax estimate
+            {spro ? (
+              // -------------main
+              <div className="product-opt w-[290px] lg:w-[460px] p-2 lg:p-8 lg:border border-[#E5E7EB] rounded-2xl h-fit">
+                <h2 className="text-2xl text-primary dark:text-white font-semibold font-popppind lg:hidden">
+                  {spro.title}
+                </h2>
+                <div className="w-full flex justify-between items-start mt-3">
+                  <div className="flex items-center gap-1 order-2 lg:order-0 w-full">
+                    <FaStar className="text-[#FBBF24]" />
+                    <p className="text-[12px] lg:text-base text-secoundary dark:text-darkSecoundart font-semibold font-popppind">
+                      {spro.rating}{" "}
+                      <span className="hidden lg:inline-block">
+                        . {} reviews{" "}
+                      </span>{" "}
+                      <span className="lg:hidden">(98)</span>
                     </p>
                   </div>
-                  <div className="r-text mb-[10px] text-end">
-                    <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
+
+                  <div className="price lg:text-end order-1 lg:order-0 mb-6 lg:mb-0">
+                    <h2 className="text-[20px] lg:text-2xl text-primary dark:text-white font-semibold font-popppind">
+                      $
+                      {Math.round(
+                        spro.price -
+                          (spro.price * spro.discountPercentage) / 100
+                      )}
+                    </h2>
+                    <h3 className="text-[12px] lg:text-sm text-secoundary dark:text-darkSecoundart font-medium font-popppind line-through">
+                      ${spro.price}
+                    </h3>
+                  </div>
+                </div>
+
+                {/* -----Product Size----- */}
+
+                <h2 className="text-base text-primary dark:text-white font-semibold font-popppind">
+                  Size: S
+                </h2>
+                <div className="flex flex-wrap gap-2 mt-3 mb-8 text-primary dark:text-white">
+                  <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
+                    S
+                  </button>
+                  <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
+                    M
+                  </button>
+                  <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
+                    L
+                  </button>
+                  <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
+                    XL
+                  </button>
+                  <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-border rounded-xl">
+                    2XL
+                  </button>
+                </div>
+
+                {/* -------Add To Cart Option---- */}
+                <div className="flex justify-between">
+                  {/* QTY */}
+                  <div className="py-[2px] lg:py-2 px-[6px] lg:px-3 rounded-[9999px] bg-border dark:bg-primary dark:border dark:border-border flex gap-4 items-center">
+                    <button className="w-5 lg:w-6 h-5 lg:h-6 rounded-full border border-border bg-white dark:bg-primary flex justify-center items-center">
+                      <FaMinus className="text-[12px] lg:text-sm text-primary dark:text-white font-normal" />
+                    </button>
+                    <h2 className="text-base text-primary dark:text-white font-medium font-popppind">
+                      1
+                    </h2>
+                    <button className="w-5 lg:w-6 h-5 lg:h-6 rounded-full border border-border bg-white dark:bg-primary flex justify-center items-center">
+                      <FaPlus className="text-[12px] lg:text-sm text-primary dark:text-white" />
+                    </button>
+                  </div>
+
+                  {/* Cart button */}
+
+                  <button className="py-[10px] lg:py-4 px-[24px] lg:px-8 bg-primary text-sm lg:text-base text-white font-medium font-popppind rounded-[9999px] dark:border dark:border-border flex justify-center items-center gap-2">
+                    <HiShoppingBag />
+                    Add to cart
+                  </button>
+                </div>
+
+                <div className="hidden lg:block">
+                  <div className="det flex items-center justify-between mt-8 pb-4 border-b border-b-border">
+                    <div className="l-text mb-[10px]">
+                      <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
+                        $169.99 x 1
+                      </p>
+                      <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
+                        Tax estimate
+                      </p>
+                    </div>
+                    <div className="r-text mb-[10px] text-end">
+                      <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
+                        $169.99
+                      </p>
+                      <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
+                        $0
+                      </p>
+                    </div>
+                  </div>
+                  <div className="total flex items-center justify-between mt-4">
+                    <h2 className="text-base text-primary dark:text-white font-semibold font-popppind">
+                      Total
+                    </h2>
+                    <h2 className="text-base text-primary dark:text-white font-semibold font-popppind">
                       $169.99
-                    </p>
-                    <p className="text-base text-secoundary dark:text-darkSecoundart font-normal font-popppind">
-                      $0
-                    </p>
+                    </h2>
                   </div>
                 </div>
-                <div className="total flex items-center justify-between mt-4">
-                  <h2 className="text-base text-primary dark:text-white font-semibold font-popppind">
-                    Total
-                  </h2>
-                  <h2 className="text-base text-primary dark:text-white font-semibold font-popppind">
-                    $169.99
-                  </h2>
-                </div>
               </div>
-            </div>
+            ) : (
+              // --------skeliton
+              <div></div>
+            )}
           </div>
 
           {/* ---Product description---- */}
@@ -281,7 +289,10 @@ const ProductDetails = () => {
               </h2>
               <div className="flex flex-wrap items-center gap-2">
                 {spro.tags?.map((item, key) => (
-                  <div key={key} className="px-[14px] py-2 flex border border-border rounded-full mt-2">
+                  <div
+                    key={key}
+                    className="px-[14px] py-2 flex border border-border rounded-full mt-2"
+                  >
                     <BsStars className="text-primary dark:text-white" />
                     <p className="text-[12px] text-secoundary dark:text-darkSecoundart font-normal font-popppind">
                       {item}
